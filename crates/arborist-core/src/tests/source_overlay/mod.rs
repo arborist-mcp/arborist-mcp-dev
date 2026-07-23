c4601ae1f3ce3565316e0105c73bea63fd03056a
TheSkyC <0x4fe6@gmail.com>
2026-07-24 03:02:23 +0800

    test(core): split source_overlay suite into thematic modules

---
+++ crates/arborist-core/src/tests/source_overlay/mod.rs
@@ -0,0 +1,19 @@
+use std::fs;
+
+use rusqlite::Connection;
+
+use super::support::{normalize_string_path, temporary_dir};
+use crate::{
+    Position, SymbolQueryContext, TraceDirection, list_symbols_from_index_with_source_filtered,
+    read_symbol_context_from_index_with_source, rebuild_symbol_index,
+    search_symbols_from_index_with_source_filtered,
+    trace_symbol_graph_at_position_from_index_with_source,
+    trace_symbol_graph_from_index_with_source, validate_patch_with_trace_context_from_index,
+    validate_patch_with_trace_context_from_path,
+};
+
+mod core;
+mod constructors;
+mod wrappers;
+mod std_get;
+
